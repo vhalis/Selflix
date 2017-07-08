3e9f5b65451eb8c1eba190d7de8c15ad7b3f535d
Daniel Manary <dmanary@gmail.com>
2017-07-08 13:37:41 -0400

    Rebuild repo

---
+++ src/components/AddNew.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class AddNew extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // Do something with a callback to create new?
+    }
+
+    render() {
+        return <div>Hello {this.props.name}</div>;
+    }
+}
+
+export default AddNew;
---
+++ stories/AddNew.js
@@ -0,0 +1,7 @@
+import AddNew from '../src/components/AddNew.jsx';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('AddNew', module)
+  .add('with text', () => <AddNew name="Text"></AddNew>)
+  .add('without text', () => <AddNew></AddNew>);
