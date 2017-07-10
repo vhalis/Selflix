10f7e7d7a36eb2c47cae04b3bcdc3cb07bf70a86
Daniel Manary <dmanary@gmail.com>
2017-07-10 08:50:12 -0400

    Finished off MovieModal

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from 'semantic-ui-react';
+import { Modal, Image, Header } from 'semantic-ui-react';
 
 
 export default class MovieModal extends React.Component {
 
     static propTypes = {
         onClose: PropTypes.func.isRequired,
+
+        movieActors: PropTypes.arrayOf(PropTypes.string),
+        movieDescription: PropTypes.string,
+        movieGenres: PropTypes.arrayOf(PropTypes.string),
+        movieImage: PropTypes.string,
+        movieReleaseDate: PropTypes.string,
+        movieTitle: PropTypes.string.isRequired,
     }
 
     state = {
@@ -20,17 +27,30 @@ export default class MovieModal extends React.Component {
 
     render() {
         const { isOpen } = this.state;
+        const actorList = this.props.movieActors.join(", ");
+        const genreList = this.props.movieGenres.join(", ");
 
         return (
             <Modal
-                basic
                 open={isOpen}
                 onClose={this.onClose}
                 dimmer='blurring'
                 size='fullscreen'
                 >
-                <Modal.Content>
-                    {this.props.children}
+
+                <Modal.Content image>
+                    <Image src={this.props.movieImage} wrapped />
+                    <Modal.Description>
+                        <Header>{this.props.movieTitle}
+                            <Header sub>{this.props.movieReleaseDate}</Header>
+                        </Header>
+                        <Header sub>Synopsis</Header>
+                        <p>{this.props.movieDescription}</p>
+                        <Header sub>Starring</Header>
+                        <p>{actorList || "Not available"}</p>
+                        <Header sub>Genres</Header>
+                        <p>{genreList || "Not available"}</p>
+                    </Modal.Description>
                 </Modal.Content>
             </Modal>
         );
